6ff82ce1d4002ac7323bb7ed770a8f50729c4001
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-05-10 18:11:26 -0400

    created error handler function

---
+++ backend/src/middleware/errorMiddleware.ts
@@ -0,0 +1,11 @@
+import { ErrorRequestHandler } from 'express';
+
+// error handler
+export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
