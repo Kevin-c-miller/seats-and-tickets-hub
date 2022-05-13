52ec9d1669f5bd1229ff60a57ac439532a490357
Kevin Miller <kevinmiller@Apples-MacBook-Pro.local>
2022-05-13 19:06:31 -0400

    added a military to standard time conversion function

--- frontend/components/AllEvents.tsx
+++ frontend/components/AllEvents.tsx
@@ -7,17 +7,30 @@ interface Props {
 }
 
 const AllEvents = ({ title, events }: Props) => {
-  // TODO: grab user long/latitude for location and render local events (if any, if not, just show generic events for US)
+  // TODO: render local events (if any, if not, just show generic events for US)
+
+  // convert time miliary time to standard time
+  const timeConversion = (militaryTime: string) => {
+    const [hours, minutes, seconds] = militaryTime.split(':');
+    return `${+hours > 12 ? +hours - 12 : hours}:${minutes}${
+      seconds ? `:${seconds}` : ''
+    } ${+hours >= 12 ? 'PM' : 'AM'}`;
+  };
 
   return (
     <div>
       <h1>{title}</h1>
 
-      <div>
-        {events.map((event) => (
+      {events.map((event) => (
+        <div key={event?.id}>
           <h3>{event?.name}</h3>
-        ))}
-      </div>
+          <h5>{event?.dates?.start?.localDate}</h5>
+          <h5>{timeConversion(event?.dates?.start?.localTime)}</h5>
+
+          {/* TODO: link to own page with more event details */}
+          {/*  <EventDetails event={event} />  */}
+        </div>
+      ))}
     </div>
   );
 };
--- frontend/services/requests.ts
+++ frontend/services/requests.ts
@@ -1,6 +1,8 @@
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 const BASE_URL = 'https://app.ticketmaster.com/discovery/v2/';
 
+// TODO:  user long/latitude for location for local events to user
+
 // structure requests as an object and export just the one object
 const requests = {
   getAllEvents: `${BASE_URL}events.json?countryCode=US&apikey=${API_KEY}`,
